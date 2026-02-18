2320dbacc84f3e6fca7093057002a4fa66969e29
sebiker <sebastianbia12@gmail.com>
2026-02-18 19:16:30 +0200

    added packages for node and set a fullName function

---
+++ sheet1.js
@@ -0,0 +1,7 @@
+// Function declaration
+function fullName(first, last) {
+    return first + " " + last;
+}
+console.log(fullName("John", "Doe"));
+console.log(fullName("Jane", "Smith"));
+console.log(fullName("Alice", "Johnson"));
---
+++ sheet1.ts
@@ -0,0 +1,8 @@
+// Function declaration
+function fullName(first: string, last: string): string {
+    return first + " " + last;
+}
+
+console.log(fullName("John", "Doe"));
+console.log(fullName("Jane", "Smith"));
+console.log(fullName("Alice", "Johnson"));
