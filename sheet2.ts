ae3e55b2de3467e025778b57d9a77e0c8c10d209
sebiker <sebastianbia12@gmail.com>
2026-02-20 18:45:58 +0200

    added new sheets

---
+++ sheet2.ts
@@ -0,0 +1,25 @@
+function SumResult(a: number, b: number): number {
+    return a + b;
+}
+
+function SubtractResult(a: number, b: number): number {
+    return a - b;
+}
+
+function MultiplyResult(a: number, b: number): number {
+    return a * b;
+}
+
+function DivideResult(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
+
+export { SumResult, SubtractResult, MultiplyResult, DivideResult };
+
+console.log(SumResult(5, 3)); // Output: 8
+console.log(SubtractResult(5, 3)); // Output: 2
+console.log(MultiplyResult(5, 3)); // Output: 15
+console.log(DivideResult(5, 3)); // Output: 1.6666666666666667
